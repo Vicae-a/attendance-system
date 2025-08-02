98f82f1c8016ef245524a724ec5b102a263a5aff
vicae-a <victoloeboss@gmail.com>
2025-08-02 19:16:39 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react(), tailwindcss()],
   resolve: {
     // Ensure .jsx/.tsx files are resolved
